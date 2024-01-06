593519bdef4f6459db1e4050e45a134dc199b411
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-06 21:41:55 +0530

    removed frame

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        frame: false,
         webPreferences: {
             nodeIntegration: true
         }
