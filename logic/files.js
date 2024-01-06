2132237f52938634e48ab45a606ffcb50469ce10
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-06 23:07:57 +0530

    x change

--- logic/files.js
+++ logic/files.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-document.getElementById('file-create').addEventListener('click', () => {
+document.getElementById('create-file').addEventListener('click', () => {
     fs.writeFileSync('output.txt', 'this is it', 'utf-8', (err) => {
         if (err) {
             alert(err)
